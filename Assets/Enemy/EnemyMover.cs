1105d5467dadfd535da8b857b006607965b8657e
AndreD4 <andremai629@gmail.com>
2023-04-11 15:37:40 +0900

    start and end position for enemy to travel

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -4,24 +4,29 @@ using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
-    [SerializeField] List<WayPoint> path = new List<WayPoint>();
-    [SerializeField] float waitTime = 1f;
+  [SerializeField] List<WayPoint> path = new List<WayPoint>();
+  [SerializeField] float waitTime = 1f;
 
-    void Start()
-    {
-      StartCoroutine (FollowPath());
-    }
+  void Start()
+  {
+    StartCoroutine(FollowPath());
+  }
 
-   IEnumerator FollowPath()
-   {
-    
-    foreach(WayPoint waypoint in path)
+  IEnumerator FollowPath()
+  {
+
+    foreach (WayPoint waypoint in path)
     {
       Vector3 startPosition = transform.position;
       Vector3 endPosition = waypoint.transform.position;
+      float travelPercent = 0f;
 
-      transform.position = waypoint.transform.position;
-      yield return new WaitForSeconds(waitTime);
+      while (travelPercent < 1f)
+      {
+        travelPercent += Time.deltaTime;
+        transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+        yield return new WaitForEndOfFrame();
+      }
     }
-   }
+  }
 }
