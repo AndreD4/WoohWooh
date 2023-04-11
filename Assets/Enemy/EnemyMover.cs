0614e50c418bd107fc3873fea276d3130c4d4737
AndreD4 <andremai629@gmail.com>
2023-04-11 14:40:44 +0900

    Enemy follows any path we input

--- Assets/Enemy/EnemyMover.cs
+++ Assets/Enemy/EnemyMover.cs
@@ -5,20 +5,20 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<WayPoint> path = new List<WayPoint>();
+    [SerializeField] float waitTime = 1f;
 
     void Start()
     {
-      Debug.Log("Start here");
       StartCoroutine (FollowPath());
-      Debug.Log("Finish here");  
     }
 
    IEnumerator FollowPath()
    {
+    
     foreach(WayPoint waypoint in path)
     {
-      Debug.Log(waypoint.name);
-      yield return new WaitForSeconds(1f);
+      transform.position = waypoint.transform.position;
+      yield return new WaitForSeconds(waitTime);
     }
    }
 }
